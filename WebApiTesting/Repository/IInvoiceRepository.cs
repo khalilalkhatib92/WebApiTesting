86c4656d4cf0a648d7486c41be667cb207f3181d
khalil al khatib <3178@rms.ps>
2021-12-16 14:40:58 +0200

    Define Invoice Repository

--- WebApiTesting/Repository/IInvoiceRepository.cs
+++ WebApiTesting/Repository/IInvoiceRepository.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiTesting.Models;
+using WebApiTesting.ViewModel;
 
 namespace WebApiTesting.Repository
 {
     public interface IInvoiceRepository
     {
+        Task<List<Currency>> GetCurrencies();
+        Task<List<Customer>> GetCustomers();
+        Task<List<Item>> GetItems();
+        Task<List<InvoiceViewModel>> GetInvoiceViewModels();
+        Task<InvoiceViewModel> GetInvoice();
 
     }
 }
--- WebApiTesting/Repository/InvoiceRepository.cs
+++ WebApiTesting/Repository/InvoiceRepository.cs
@@ -1,11 +1,48 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiTesting.Models;
+using WebApiTesting.ViewModel;
 
 namespace WebApiTesting.Repository
 {
     public class InvoiceRepository : IInvoiceRepository
     {
+        ApiDbContext db;
+        public InvoiceRepository(ApiDbContext _db)
+        {
+            db = _db;
+        }
+
+        public async Task<List<Currency>> GetCurrencies()
+        {
+            if (db != null)
+            {
+                return await db.Currencies.ToListAsync();
+            }
+            return null;
+        }
+
+        public Task<List<Customer>> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InvoiceViewModel> GetInvoice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<InvoiceViewModel>> GetInvoiceViewModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Item>> GetItems()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
